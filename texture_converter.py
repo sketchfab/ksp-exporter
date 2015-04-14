3c02e58ab882f647bee6bf2ef5566c56cf59e13a
Aurélien Chatelain <aurelien.chatelain@utbm.fr>
2015-04-14 10:23:47 +0200

    [#91768704] Adds heightmap to normal map conversion
    Since the data to identify which textures need to
    be converted to normal maps is in the MBM header, we also perform the
    heightmap to normal map conversion.

--- texture_converter.py
+++ texture_converter.py
@@ -1,19 +1,35 @@
 # -*- coding: utf8 -*-
 import png
 from struct import unpack
-import sys
 import os
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 import tempfile
 
+
 class Converter(object):
+    def convert_bump(self, pixels, width, height, nb_components):
+        outp = []
+        for y in range(1, height - 1):
+            w = []
+            for x in range(1, (width * nb_components) - 1, nb_components):
+                vdx = QtGui.QVector3D(1, 0, (pixels[y][x+1] - pixels[y][x-1]) / 60.0)
+                vdy = QtGui.QVector3D(0, 1, (pixels[y+1][x] - pixels[y-1][x]) / 60.0)
+                vdz = QtGui.QVector3D.normal(vdx, vdy)
+                vdz = vdz.__mul__(QtGui.QVector3D(127, 127, 127))
+                vdz = vdz.__add__(QtGui.QVector3D(128, 128, 128))
+                w.append(vdz.x())
+                w.append(vdz.y())
+                w.append(vdz.z())
+            outp.append(w)
+        return outp
+
     def load_mbm(self, mbmpath):
         mbmfile = open(mbmpath, "rb")
         header = mbmfile.read(20)
         magic, width, height, bump, bpp = unpack("<5i", header)
         img_format = 'RGBA'
         pixels = []
-        if magic != 0x50534b03: # "\x03KSP" as little endian
+        if magic != 0x50534b03:  # "\x03KSP" as little endian
             raise
         if bpp == 32:
             for j in range(height):
@@ -38,6 +54,9 @@ class Converter(object):
         else:
             raise
 
+        if bump and len(pixels) > 2:
+            pixels = self.convert_bump(pixels, width, height, bpp/8)
+            img_format = 'RGB'
         output = tempfile.gettempdir()
         subfolder = os.path.split(os.path.split(os.path.splitdrive(mbmpath)[1])[0])[1]
         output = os.path.join(output, subfolder)
@@ -48,4 +67,3 @@ class Converter(object):
         png.from_array(pixels, img_format).save(os.path.join(output, filename))
 
         return os.path.join(output, filename)
-
