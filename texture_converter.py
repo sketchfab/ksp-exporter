8ffc45695e302926ad6d13852bf652dbd9200920
Aurélien Chatelain <aurelien.chatelain@utbm.fr>
2015-04-17 10:36:06 +0200

    [#91768006] Improves textures conversion
    The conversion now uses PIL functions to convert more efficiently
    textures from MBM to PNG, also handling heightmap to normal map conversion.

--- texture_converter.py
+++ texture_converter.py
@@ -1,68 +1,69 @@
 # -*- coding: utf8 -*-
-import png
 from struct import unpack
+from math import sqrt
+from PIL import Image, ImageFilter, ImageChops
 import os
 import tempfile
-import math
-
-
-# Utils
-def normalize(vect):
-    ''' Normalize vector '''
-    norm = math.sqrt(sum(map(lambda x: math.pow(x, 2), vect)))
-    new_vect = map(lambda x: x/float(norm), vect)
-    return new_vect
 
 
 class Converter(object):
-    def convert_bump(self, pixels, width, height, nb_components):
-        outp = []
+    # Code snippet from http://www.pythonstuff.org/glsl/normalmaps_from_heightmaps.html
+    def height2bump(self,  heightBand ): # normal[0..2] band-array
+        global verbose
+        r = ImageChops.constant(heightBand, 128)
+        g = ImageChops.constant(heightBand, 128)
+        b = ImageChops.constant(heightBand, 128)
+        rr=r.load()
+        gg=g.load()
+        bb=b.load()
+        width = heightBand.size[0]
+        height = heightBand.size[1]
+        data = list(heightBand.getdata())
+        bits = 256
         for y in range(1, height - 1):
-            w = []
-            for x in range(1, (width * nb_components) - 1, nb_components):
-                norm_val = [pixels[y][x+1] - pixels[y][x-1],
-                            pixels[y+1][x] - pixels[y-1][x], 255]
-                norm_val = normalize(norm_val)
-                norm_val = map(lambda x: x*127, norm_val)
-                norm_val = map(lambda x: x+128, norm_val)
-                w.extend(norm_val)
-            outp.append(w)
-        return outp
+            ypos = y * width
+            for x in range(1, width - 1):
+                pixel = (ypos + x)
+                pixelL = pixel - 1
+                pixelR = pixel + 1
+                pixelT = pixel - width
+                pixelB = pixel + width
+                dx = -((data[pixelR] - data[pixelL]) / (bits *2.0))
+                dy = -((data[pixelT] - data[pixelB]) / (bits *2.0))
+                l = sqrt(1 + dx**2 + dy**2)
+                dz = 1  / l
+                dx = dx / l
+                dy = dy / l
+
+                #rgba  tex direct
+                #pixelPos = pixel * 4
+                #outp[pixel * 4] = dx
+                #outp[pixel ] = dy
+                #outp[pixel + 2] = dz
+                rr[x, y] = dx*bits + 128
+                gg[x, y] = dy*bits + 128
+                bb[x, y] = dz*bits + 128
+        return([r, g, b])
+
+
+    def readHeight2Bump(self, input_image):
+        height = input_image.split()[0]
+        normal = self.height2bump(height)
+        input_image = Image.merge('RGB', normal)
+
+        return input_image
 
     def load_mbm(self, mbmpath):
         mbmfile = open(mbmpath, "rb")
         header = mbmfile.read(20)
         magic, width, height, bump, bpp = unpack("<5i", header)
-        img_format = 'RGBA'
-        pixels = []
-        if magic != 0x50534b03:  # "\x03KSP" as little endian
-            raise
-        if bpp == 32:
-            for j in range(height):
-                w = []
-                for i in range(width):
-                    w.append(unpack("B", mbmfile.read(1))[0])
-                    w.append(unpack("B", mbmfile.read(1))[0])
-                    w.append(unpack("B", mbmfile.read(1))[0])
-                    w.append(unpack("B", mbmfile.read(1))[0])
-                # FIXME : why do we have to insert at the beginning ?
-                # Behaviour of the png function ?
-                pixels.insert(0, w)
-        elif bpp == 24:
-            img_format = 'RGB'
-            for j in range(height):
-                w = []
-                for i in range(width):
-                    w.append(unpack("B", mbmfile.read(1))[0])
-                    w.append(unpack("B", mbmfile.read(1))[0])
-                    w.append(unpack("B", mbmfile.read(1))[0])
-                pixels.insert(0, w)
-        else:
-            raise
+        img_format = 'RGBA' if bpp == 32 else 'RGB'
+        pixels = mbmfile.read(width * height * 4)
+        img = Image.frombuffer(img_format, (width, height), pixels, 'raw', img_format, 0, 1)
 
-        if bump and len(pixels) > 2:
-            pixels = self.convert_bump(pixels, width, height, bpp/8)
-            img_format = 'RGB'
+        # Needs to flip top/bottom in order to get the right image
+        img = img.transpose(1)
+
         output = tempfile.gettempdir()
         subfolder = os.path.split(os.path.split(os.path.splitdrive(mbmpath)[1])[0])[1]
         output = os.path.join(output, subfolder)
@@ -70,6 +71,10 @@ class Converter(object):
             os.mkdir(output)
 
         filename = os.path.splitext(os.path.basename(mbmpath))[0] + '.png'
-        png.from_array(pixels, img_format).save(os.path.join(output, filename))
+
+        if bump:
+            img = self.readHeight2Bump(img)
+
+        img.save(os.path.join(output, filename))
 
         return os.path.join(output, filename)
